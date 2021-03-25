3fc380c2fd00061b38eb48271da9980cd58b1a48
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-03-25 13:03:16 -0300

    CREATE server with node/mysql

---
+++ server/database/db.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE instaclondb;
+
+USE instaclondb;
+
+CREATE TABLE users(
+  idUser INT(11) AUTO_INCREMENT PRIMARY KEY,
+  userName VARCHAR(16) NOT NULL,
+  password VARCHAR(60) NOT NULL
+);
+
+DESCRIBE users;
+
+CREATE TABLE photo (
+  idPhoto INT(11) AUTO_INCREMENT PRIMARY KEY,
+  url VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  idUser INT(11) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT current_timestamp,
+  CONSTRAINT fk_user FOREIGN KEY (idUser) REFERENCES users(idUser)
+);
+
+DESCRIBE photo;
